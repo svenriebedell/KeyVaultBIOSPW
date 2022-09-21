9dcab98241a88a7ed77ee75c5b4a18ecf5654507
Sven Riebe @ Dell <99394991+svenriebedell@users.noreply.github.com>
2022-09-21 14:42:47 +0200

    temp

---
+++ keyvault.ps1
@@ -0,0 +1,7 @@
+﻿$securevalue = ConvertTo-SecureString "Dell9000!" -AsPlainText -Force
+$secret =  Set-AzureKeyVaultSecret -VaultName "pwdbios" -Name "1X752J3" -SecretValue $securevalue
+
+
+<#Install Az.Accounts
+Install-Module -Name Az#>
+Install-Module -Name Az.KeyVault -Force
