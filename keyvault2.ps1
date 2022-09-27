a188cb70cb85ab5551c715e6e79f70d0d867776f
Sven Riebe @ Dell <99394991+svenriebedell@users.noreply.github.com>
2022-09-27 13:53:36 +0200

    Update keyvault2.ps1

--- keyvault2.ps1
+++ keyvault2.ps1
@@ -3,7 +3,7 @@ $App_ID = "9bd11b68-222e-4a0f-941a-a12345c3957d"
 $ThumbPrint = "52FADBF9B36685E734DA84C6283D9EA7D4302E35"
 Connect-AzAccount -tenantid $TenantID -ApplicationId $App_ID -CertificateThumbprint $ThumbPrint
 
-$secret = (Get-AzKeyVaultSecret -vaultName "PWDBIOS" -name "1X752J3") | select *
+$secret = (Get-AzKeyVaultSecret -vaultName "PWDBIOS" -name "SRLAB-1X752J3") | select *
 $Get_My_Scret = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($secret.SecretValue) 
 $Display_My_Secret = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($Get_My_Scret) 
 $Display_My_Secret
