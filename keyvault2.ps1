718934453fec85c47ef1c45a1a7cec358b01f90d
Sven Riebe @ Dell <99394991+svenriebedell@users.noreply.github.com>
2022-10-07 08:50:19 +0200

    Delete keyvault2.ps1

--- keyvault2.ps1
+++
@@ -1,9 +0,0 @@
-﻿$TenantID = "0d02d0b5-802d-427e-abc8-5166348b7f1f"
-$App_ID = "9bd11b68-222e-4a0f-941a-a12345c3957d"
-$ThumbPrint = "52FADBF9B36685E734DA84C6283D9EA7D4302E35"
-Connect-AzAccount -tenantid $TenantID -ApplicationId $App_ID -CertificateThumbprint $ThumbPrint
-
-$secret = (Get-AzKeyVaultSecret -vaultName "PWDBIOS" -name "SRLAB-1X752J3") | select *
-$Get_My_Scret = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($secret.SecretValue) 
-$Display_My_Secret = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($Get_My_Scret) 
-$Display_My_Secret
